3308943a676b39b3c1ead126f0e58af76534b4a3
Tom Tresansky <ttresansky@gradle.com>
2025-09-22 11:36:54 -0400

    Downgrade to Java 17

--- build-logic/conventions/src/main/kotlin/java-lib-conventions.gradle.kts
+++ build-logic/conventions/src/main/kotlin/java-lib-conventions.gradle.kts
@@ -4,10 +4,10 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 
     toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
+        languageVersion = JavaLanguageVersion.of(17)
     }
 }
